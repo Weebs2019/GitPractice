4e373c10a560aaf0b6a2f79bfb02a13f6d11efa1
Stephen Wibowo <wibowo.4@wright.edu>
2021-06-30 18:24:07 -0400

    Add project files.

---
+++ GitPractice/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetNameOfFiles
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] fileArray = Directory.GetFiles(@"C:\Users\swibowi\Documents\");
+            for (int i = 0; i < fileArray.Length; i++)
+            {
+
+                Console.WriteLine(fileArray[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
